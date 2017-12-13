19fb18de57925a4199c12966a1412ab483225b38
Mabeesha <mabeesharj@gmail.com>
2017-12-13 22:39:01 +0530

    Patient Login done
    Some changes are done.
    Do the follwings;
       my data base name  - baymax
       patients' tabele - patient_details
    1)change that according to your data base
    2)add a column called "password" in patient_details table.

--- patientlogin.php
+++ patientlogin.php
@@ -1,34 +1,32 @@
-<?php	
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-
-	// Create connection
-	$conn = new mysqli($servername, $username, $password);
-
-	// Check connection
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	} 
-	//echo "Connected successfully";
-
-	if(strlen($_GET['username']) > 0){
-				//echo "<p> Thank you, ".$_GET['username'].' for your purchase from our web site</p>' ;
-			$user = $_GET['username'];
-			$pass = $_GET['password'];
-	}
-	//$sql = "INSERT INTO baymax.patientlogin (username, password) VALUES ('$user', '$pass')";
-
-	/*if ($conn->query($sql) === TRUE) {
-	   // echo "<br>New record created successfully";*/
-		echo file_get_contents("PatientHomePage.html");
-		/*$conn->close();
-	} else {
-	    echo "Error: " . $sql . "<br>" . $conn->error;
-		$conn->close();	
-	}*/
-
-
-
-
+<?php
+   include("config.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT patientusername FROM patient_details WHERE patientusername = '$myusername' and password = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      //$active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+    
+      if($count == 1) {
+         $_SESSION['username'] = $myusername;
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: patientHomePage.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+         //echo "alert('Hello World!')";
+         header("location:patientLogin2.html");
+      }
+   }
 ?>
+
